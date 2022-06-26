44671ba8b1ec953e71d3316e5d146b2c64cd600c
Киселев Алексей Борисович <a.b.kiselev@yandex.ru>
2022-06-26 21:00:22 +0300

    Резерв

--- src/components/AddGif.js
+++ src/components/AddGif.js
@@ -15,6 +15,10 @@ export default class AddGif {
   }
 
   replaceGif(newGif) {
-    this._containerElement.replaceChild(newGif, this._containerElement.firstChild)
+    // this._containerElement.replaceChild(newGif, this._containerElement.firstChild)
+    if(this._containerElement.firstChild){
+      this._containerElement.firstChild.replaceWith(newGif)
+    } this._containerElement.append(newGif)
+    
   }
 }
